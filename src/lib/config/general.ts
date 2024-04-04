cf61e0c76028617dc6745c6dd78354e6af93da75
webwizarding <162193936+webwizarding@users.noreply.github.com>
2024-04-04 00:09:55 -0700

    Update general.ts

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -56,8 +56,8 @@ export const head: HeadConfig = {}
 export const header: HeaderConfig = {
   nav: [
     {
-      text: 'Get Started',
-      link: '/hello-world'
+      text: 'Argonhosting',
+      link: '/Argonhosting
     },
     {
       text: 'Roblox OSINT',
