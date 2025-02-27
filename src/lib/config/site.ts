7d0566848651984ca5c71575323e0cd454cad30c
webwizarding <162193936+webwizarding@users.noreply.github.com>
2025-02-27 14:15:25 -0800

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Poly',
+  title: 'shit',
   subtitle: 'blog',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
